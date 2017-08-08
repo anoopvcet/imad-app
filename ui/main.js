94ba1e25c45f69bdbfbd11b584d653dad6d30d65
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 22:34:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,7 +36,7 @@ button.onclick=function(){
         
     };
     
-    request.open('GET', "http://anoopvcet.imad.hasura-app.io/counter",true);
+    request.open('GET', 'http://anoopvcet.imad.hasura-app.io/counter',true);
     request.send(null);
     
 };
