9a407c61c893824a7f2e6d16db3e1d7809a9c1ff
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-09 00:17:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -89,7 +89,7 @@ var name =nameInput.value;
    var submit =document.getElementById('submit_btn');
 submit.onclick = function()
 {
-    var names =['name1'];
+    var names =['name1','NAME2','name3','name4'];
     var list='';
     for(var i=0 ; i<names.length ; i++)
     {
