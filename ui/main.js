5efffea1789cdfd8ea0521f236d93b71ddc329fe
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 23:37:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -81,3 +81,23 @@ button.onclick=function(){
     request.send(null);
     
 };
+
+
+
+var nameinput= document.getElementById('name');
+var name =nameinput.value;
+var submit =document.getElementById('submit_btn');
+submit.onclick = function()
+{
+    var names =['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0; i<names.length; i++)
+    {
+      list+='<li>'+name[i] + '<li>';  
+    }
+    
+    var ul= document.getElementById('namelist');
+    ul.innerHTML= 'list';
+};
+
+
