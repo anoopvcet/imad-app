bee9c7a4a0612d914b34d01157adc38b8aeaa0ed
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 11:22:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var articles={
     content:`<p>
     <h1> Malala Yousafzai </h1>
     <br>
-    <h1> masala</h1>
+    <h1>Women's Rights Activist, Children's Activist, Activist from 1997</h1>
                     As a young girl, Malala Yousafzai defied the Taliban in Pakistan and demanded that girls be allowed to receive an education. She was shot in the head by a Taliban gunman in 2012, but survived and went on to receive the Nobel Peace Prize.
 
                 </p>
