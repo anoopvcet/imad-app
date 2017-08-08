414ef7701d3518fcd72a2f9f5404cc949983c45e
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 22:41:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,4 @@
-console.log('Loaded!');
-console.log('how are you');
+
 
 var img=document.getElementById('madi');
 
@@ -19,17 +18,59 @@ img.onclick= function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var button = document.getElementById('counter');
 button.onclick=function(){
     var request= new XMLHttpRequest();
     
-    request.onreadystatechange = function(){
+    request.onreadystatechange = function()
+    {
      if (request.readyState===  XMLHttpRequest.DONE)
      {
-         if (request.status===200){
+         if (request.status===200)
+         {
              var counter=request.responseText;
              var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+             span.innerHTML=counter.toString();
          }
          
      } 
