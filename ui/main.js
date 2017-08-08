b76afb43297b18a3bbaa3413a7861476a5690f7a
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 20:05:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,14 +2,15 @@ console.log('Loaded!');
 console.log('how are you');
 
 var img=document.getElementById('madi');
+
 var marginLeft =0;
 function moveRight(){
     marginLeft+=5;
-    img.style.marginLeft+= 'px';
+    img.style.marginLeft = marginLeft + 'px';
     
 }
 img.onclick= function(){
     
-    var interval= setInterval(moveRight , 100);
+    var interval= setInterval(moveRight , 50);
     
 };
