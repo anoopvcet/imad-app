5060ed9bbceee02b4becab0505d8622d53ea00dc
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 21:44:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,7 +14,7 @@ img.onclick= function(){
     var interval= setInterval(moveRight , 50);
     
 };
-
+var counter=0;
 var button = document.getElementById('counter');
 button.onclick=function(){
     
