3bb7492e9cd8e8a4e9efd38f0d9e3483cbb10f04
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 21:26:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -299,6 +299,14 @@ var htmltemplate =
     return htmltemplate;
 }
 
+
+var counter=0;
+app.get('/counter', function(req,res){
+counter+=1;
+res.send(counter.toString());
+    
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
