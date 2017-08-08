4b26b18d3727f91da46f7cddaf5ecd216a0c56d7
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-08 11:42:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,13 @@ On October 9, 2012, when 15year old Malala was riding a bus with friends on thei
 <br>
 The shooting left Malala in critical condition, so she was flown to a military hospital in Peshawar. A portion of her skull was removed to treat her swelling brain. To receive further care, she was transferred to Birmingham, England.
 
-</p>`
+</p>
+<p>
+<h1>>After the Attack</h1
+
+Once she was in the United Kingdom, Yousafzai was taken out of a medically induced coma. Though she would require multiple surgeries including repair of a facial nerve to fix the paralyzed left side of her face she had suffered no major brain damage. In March 2013, she was able to begin attending school in Birmingham.
+
+The shooting resulted in a massive outpouring of support for Yousafzai, which continued during her recovery. She gave a speech at the United Nations on her 16th birthday, in 2013. She has also written an autobiography, I Am Malala: The Girl Who Stood Up for Education and Was Shot by the Taliban, which was released in October 2013. Unfortunately, the Taliban still considers Yousafzai a target.</p>`
     
 },
  'article-two':{title:'article two |Anoop V',
