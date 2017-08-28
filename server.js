5b92e14e7543965ee8dc206a2bb78a36a60940a0
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-28 08:46:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -295,7 +295,7 @@ var htmltemplate =
                 ${heading}
             </h3>
             <div>
-                ${date}
+                ${date.toDateString()}
             </div>
             <div>
                ${content}
