edf47f10e54b20b6da69b85252d64a4f87af7781
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-28 21:13:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool =require('pg').Pool;
 var crypto = require('crypto');
-
+var bodyParser= require('body-parser');
 
 
 
@@ -19,6 +19,9 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.JSON())
+
+
 
 var articles={
  'article-one': {
@@ -347,11 +350,62 @@ res.send(JSON.stringify(names));
 
 function hash(input, salt){
   var hashed = crypto.pbkdf2Sync(input,salt,1000,512, 'sha512');
-return hashed.toString('hex');
+return ['pbkdf2','1000',salt, hashed.toString('hex')].join('$');
 }
 
 
+app.post('/create-user',function(req,res){
+    var username= req.body.username;
+    var password= req.body.password;
+    var salt = crypto.RandomBytes(128).toString('hex');
+    var dbString= hash(password,salt);
+    pool.query('INSERT INTO "user" (username, password) VALUES($1,$2)', [username,dbString], function(err,result){
+        
+        if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           
+               res.send('User sucessfully created'+ username);
+           }
+        
+    });
+});
 
+
+app.post('/login',function(req,res){
+    var username= req.body.username;
+    var password= req.body.password;
+    
+
+    pool.query('SELECT * from "user" username= $1', function(err,result){
+        
+        if(err){
+           res.status(500).send(err.toString());
+       }
+       else{
+           if(result.rows.length===0){
+               res.send(403).send('username/password is invalid');
+           }
+           else{
+               var dbString =result.row[0].password;
+              var salt= dbString.split('$')[2];
+              
+              var hashedPassword =hash(password,salt);
+              if(hashedPassword===dbString){
+               res.send('welcome'+username);
+           }
+           else{
+               res.send(403).send('username/password is invalid');
+           }
+               
+           }
+           
+               
+           }
+        
+    });
+});
 
 
 app.get('/hash/:input', function(req,res){
