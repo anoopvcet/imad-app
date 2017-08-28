1123911ef551f49f696f9655b73b400321e46bd5
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-28 23:13:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -123,6 +123,8 @@ submit1.onclick = function()
     };
     var username= document.getElementById('username').value;
     var password= document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
     request.open('POST', 'http://anoopvcet.imad.hasura-app.io/login' ,true);
     request.setRequestHeader('content-type','application/json');
     request.send(JSON.stringify({username:username, password:password}));
