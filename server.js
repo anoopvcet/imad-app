32fa432976731d5a2212bc4e5337a5c5abebdab2
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-29 12:01:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.post('/login',function(req,res){
        }
        else{
            if(result.rows.length===0){
-               res.send(403);
+               res.status(403).send('username/password is invalid');
            }
            else{
                var dbString =result.rows[0].password;
@@ -162,7 +162,7 @@ app.post('/login',function(req,res){
                res.send('welcome');
            }
            else{
-               res.send(403);
+               res.status(403).send('username/password is invalid');
            }
                
            }
