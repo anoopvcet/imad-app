2282388ebf551490e27dfe35ae96cca880a760c4
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2017-08-29 14:04:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,14 +88,7 @@ res.send(counter.toString());
 });
 
 var Pool= new Pool(config);
-app.get('/test-db', function(req,res){
-   Pool.query('SELECT * from test', function(err,result){
-       if(err){
-           res.status(500).send(err.toString());
-       }
-       else{res.send(JSON.stringify(result.rows))}
-   }) ;
-});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -182,11 +175,10 @@ app.get('/check-login',function(req,res){
     }
 });
 
-app.get('/logout',function(req,res){
+app.get('/logout', function (req, res) {
    delete req.session.auth;
-   res.send('you are logged out');
+   res.send('<html><body>Logged out!<br/><br/><a href="/">Back to home</a></body></html>');
 });
-
 
 
 app.get('/hash/:input', function(req,res){
@@ -195,7 +187,60 @@ res.send(hashedString);
 }
 );
 
+app.get('/get-articles', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT * FROM article ORDER BY date DESC', function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
 
+app.get('/get-comments/:articleName', function (req, res) {
+   // make a select request
+   // return a response with the results
+   pool.query('SELECT comment.*, "user".username FROM article, comment, "user" WHERE article.title = $1 AND article.id = comment.article_id AND comment.user_id = "user".id ORDER BY comment.timestamp DESC', [req.params.articleName], function (err, result) {
+      if (err) {
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result.rows));
+      }
+   });
+});
+
+app.post('/submit-comment/:articleName', function (req, res) {
+   // Check if the user is logged in
+    if (req.session && req.session.auth && req.session.auth.userId) {
+        // First check if the article exists and get the article-id
+        pool.query('SELECT * from article where title = $1', [req.params.articleName], function (err, result) {
+            if (err) {
+                res.status(500).send(err.toString());
+            } else {
+                if (result.rows.length === 0) {
+                    res.status(400).send('Article not found');
+                } else {
+                    var articleId = result.rows[0].id;
+                    // Now insert the right comment for this article
+                    pool.query(
+                        "INSERT INTO comment (comment, article_id, user_id) VALUES ($1, $2, $3)",
+                        [req.body.comment, articleId, req.session.auth.userId],
+                        function (err, result) {
+                            if (err) {
+                                res.status(500).send(err.toString());
+                            } else {
+                                res.status(200).send('Comment inserted!')
+                            }
+                        });
+                }
+            }
+       });     
+    } else {
+        res.status(403).send('Only logged in users can comment');
+    }
+});
 
 
 
@@ -224,7 +269,9 @@ app.get('/article/:articleName',function(req,res){
 });
 
 
-
+app.get('/ui/:fileName', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', req.params.fileName));
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
