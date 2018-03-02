6e1054624703f500e8a813e1583b2a299116ac86
anoopvcet <30699981+anoopvcet@users.noreply.github.com>
2018-03-02 10:03:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -129,6 +129,20 @@ function loadArticles () {
     request.open('GET', '/get-articles', true);
     request.send(null);
 }
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length; i++){
+        list+='<li>'+names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+    
+};
 
 
 // The first thing to do is to check if the user is logged in!
